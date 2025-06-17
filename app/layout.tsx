fde96f2d2598b35c1a15ecaef7e90a50796d0727
Rohan Sharma <specialrohansharma200@gmail.com>
2025-06-17 14:55:55 +0530

    feat: added OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,38 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Radhika-Bot',
-  description: 'Radhika-Bot is a versatile AI chatbot designed to assist with a wide range of tasks, from answering questions to providing recommendations and engaging in casual conversation.',
+  title: 'Radhika',
+  description:
+    'Radhika is a versatile AI chatbot designed to assist with a wide range of tasks, from answering questions to providing recommendations and engaging in casual conversation.',
   generator: 'Rohan Sharma',
   icons: {
     icon: '/favicon.ico',
-  }
+  },
+  openGraph: {
+    title: 'Radhika',
+    siteName: 'radhika-sharma',
+    url: 'https://radhika-sharma.vercel.app/',
+    description:
+      'Radhika is a versatile AI chatbot designed to assist with a wide range of tasks, from answering questions to providing recommendations and engaging in casual conversation.',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Radhika AI Chatbot',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Radhika',
+    description:
+      'Radhika is a versatile AI chatbot designed to assist with a wide range of tasks, from answering questions to providing recommendations and engaging in casual conversation.',
+    images: ['/og-image.png'],
+  },
 }
+
 
 export default function RootLayout({
   children,
